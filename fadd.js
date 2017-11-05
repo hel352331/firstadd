98d307798b87e128382749dff5a3f0b05ad544df
hel352331 <alenka352331@gmail.com>
2017-11-05 10:45:41 +0200

    ready

--- fadd.js
+++ fadd.js
@@ -1,25 +1,9 @@
-// Все работает, только если ничего не ввели-остается запятая, и не хочет заносить 0
-
-// var arr = [ , 1, 2, 3, 4];
-//
-// var first = +prompt('Input your number');
-//
-//   if (!isNaN(first) && first != ' ' ){
-//     arr = [first, 1, 2, 3, 4];
-//     alert('Your array is: ' + arr);
-//   }else{
-//     alert('Input correct number');
-//   }
-//
-// OR
-
-// Все работает, кроме 0(если if), если без if-при пустом поле-заносит на первое место 0)
 var arr = [1, 2, 3, 4];
-var new_arr = [];
 var first = +prompt('Input your number');
-if (first == ' ' || isNaN(first)){
-    alert('Input correct number');
-  }else{
-    new_arr = first + ',' + arr;
-    alert('Your array is: ' + new_arr);
+var i = 0;
+while (i < arr.length) {
+  arr[arr.length - i] = arr[arr.length - i - 1]
+  i++;
 }
+arr[0] = first;
+    alert('Your array is: ' + arr);
